94a564014586eedb01288a08881c8a1023a9a8f0
Enrique Delgado <delgado.enrique@gmail.com>
2013-04-19 15:51:25 -0500

    * App now obtains a Salesforce.com REST API token on statup.

--- startup.js
+++ startup.js
@@ -14,17 +14,14 @@ sfdcAssistant.setupEventListeners = function() {
     'eventName': 'digium.phone.incoming_call',
     'callback': this.handleIncomingCall.bind(this)
   });
-  /* Erroneous observe construction (it requires a named array):
-  digium.event.observe('digium.phone.incoming_call', handleIncomingCall);
-  */
   
 };
 
 // Handle incoming phone calls.
 sfdcAssistant.handleIncomingCall = function(callData) {
-  // util.debug('=================');
+  util.debug('=================');
   util.debug('Incoming call! Call details:');
-/*
+  util.debug('Token is: ' + this.sfdcToken);
   util.debug('callHandle: ' + callData.callHandle);
   util.debug('callerId: ' + tools.getCallerId(callData.eventData).number);
   util.debug('method: ' + callData.method);
@@ -40,12 +37,62 @@ sfdcAssistant.handleIncomingCall = function(callData) {
   util.debug('lastStatusText: '    + callData.eventData.lastStatusText);
   util.debug('mediaStatus: '    + callData.eventData.mediaStatus);
   util.debug('headers: '    + callData.eventData.headers);
-*/
 };
 
-// Main app flow.
+// Gets a Salesforce.com REST API token.
+sfdcAssistant.getSFDCToken = function(){
+  var request = new NetRequest();
+  var URL = 'https://' + this.config.sfdc_instance + '.salesforce.com/services/oauth2/token';
+  request.open('POST', URL);
+  // Salesforce expects a URL-encoded POST data payload 
+  // http://www.salesforce.com/us/developer/docs/api_rest/Content/intro_understanding_username_password_oauth_flow.htm
+  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  request.oncomplete = function() {
+    var response = JSON.parse(this.responseText);
+    // util.debug(JSON.stringify(response));
+    // util.debug(response.access_token);
+    // Set the token and instance URL in the larger app scope:
+    sfdcAssistant.setSFDCToken(response);
+  };
+  var body = {
+    'grant_type':    'password',
+    'client_id':     this.config.sfdc_consumer_key,
+    'client_secret': this.config.sfdc_consumer_secret,
+    'username':      this.config.sfdc_username,
+    'password':      this.config.sfdc_password
+  };
+  // The POST data should be sent in an URL-Encoded string:
+  request.send(tools.serialize(body));
+};
+
+// Sets the SFDC token and SFDC instance URL in a place where other app methods can get to it:
+sfdcAssistant.setSFDCToken = function(obj) {
+  this.sfdcToken = obj.access_token;
+  this.sfdcInstanceURL = obj.instance_url;
+  // Test it out by printing the token to the console.
+  this.printSFDCToken();
+};
+
+sfdcAssistant.printSFDCToken = function() {
+  util.debug('Got SFDC Token! Token is: ' + this.sfdcToken);
+};
+
+// Setup steps:
 sfdcAssistant.start = function() {
   util.debug('App started on a phone model ' + digium.phoneModel);
+  // Get the config settings and cache it locally:
+  var config = app.getConfig().settings;
+  var sfdcToken = null;
+  this.config = util.defaults(config, {
+    'sfdc_consumer_key':    '',
+    'sfdc_consumer_secret': '',
+    'sfdc_username':        '',
+    'sfdc_password':        '',
+    'sfdc_instance':        'test' // test, na1, cs3, etc.
+  });
+  // Salesforce.com REST API token:
+  this.getSFDCToken();
+  // Setup event listeners:
   this.setupEventListeners();
 };
 
--- tools.js
+++ tools.js
@@ -2,6 +2,7 @@
  * Utility functions.
  */
 var tools = {};
+
 // Parse the incoming call object for caller id information
 // Credit: Sample CallerID Digium Phone App (https://github.com/sruffell/digium-phone-apps/blob/master/callerId/startup.js#L242-L251)
 tools.getCallerId = function(params) {
@@ -13,6 +14,21 @@ tools.getCallerId = function(params) {
   obj.number = parts[2].split(':')[1].split('@')[0];
   return obj;
 };
+
+// Convert a Javascript object into an URL encoded string.
+// serialize({'apple': 'red delicious', 'banana': 'yellow'}) returns: apple=red%20delicious&banana=yellow
+tools.serialize = function(obj, prefix) {
+    var str = [];
+    for(var p in obj) {
+        var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+        str.push(typeof v == "object" ? 
+            serialize(v, k) :
+            encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+    return str.join("&");
+}
+
 // Set exports as required in the "require()" core method:
 // See http://phones.digium.com/phone-api/reference/javascript-modules/exports
 exports.getCallerId = tools.getCallerId.bind(tools);
+exports.serialize   = tools.serialize.bind(tools);
